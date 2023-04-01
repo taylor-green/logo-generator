92aa1697a8093e92b3c70bb348c012ee14044cf0
taylor-green <taylor.green2804@gmail.com>
2023-04-01 12:18:04 -0400

    created render function for sqaure class and began for triangle class

--- shapes.js
+++ shapes.js
@@ -11,6 +11,17 @@ class Shape {
 class Circle extends Shape{
 
     render(){
-        return `<circ cx='50%' cy='50%' r='100' height='100%' width='100%' fill='{this.color}'/>`
+        return `<circ cx='50%' cy='50%' r='100' height='100%' width='100%' fill='${this.color}'/>`
     }
+}
+
+class Square extends Shape{
+
+    render(){
+        return `<rect x='50' height='200' width='200' fill='${this.color}'/>`
+    }
+}
+
+class Triangle extends Shape{
+    
 }
