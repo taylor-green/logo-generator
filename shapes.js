e7dd4655812ae1582e7d8d50ec04577163b3d50a
taylor-green <taylor.green2804@gmail.com>
2023-04-01 12:11:32 -0400

    created render function for circle class

--- shapes.js
+++ shapes.js
@@ -7,3 +7,10 @@ class Shape {
         this.color = (color)
     }
 }
+
+class Circle extends Shape{
+
+    render(){
+        return `<circ cx='50%' cy='50%' r='100' height='100%' width='100%' fill='{this.color}'/>`
+    }
+}
