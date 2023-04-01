6428d5f487bbb6cde1ad887ed0ad3d776c4e5877
taylor-green <taylor.green2804@gmail.com>
2023-04-01 12:22:23 -0400

    finished render function for traingle class and exported

--- shapes.js
+++ shapes.js
@@ -23,5 +23,10 @@ class Square extends Shape{
 }
 
 class Triangle extends Shape{
-    
-}
+
+    render(){
+        return `polygon height='100%' width='100%' points='0,200 300,200 150,0' fill='${this.color}'/>`
+    }
+};
+
+module.exports = {Circle, Square, Triangle}
