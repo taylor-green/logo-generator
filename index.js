27282344efaa902308b8007443ea94629b220360
taylor-green <taylor.green2804@gmail.com>
2023-04-02 17:17:16 -0400

    created svg class and render function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+const fs = require('./node_modules/graceful-fs/graceful-fs');
 const inquirer = require('inquirer');
-const fs = require('fs');
 const {Circle, Square, Triangle} = require('./lib/shapes');
 
 class Svg{
@@ -8,11 +8,19 @@ class Svg{
         this.shapeElement = ''
     }
     render(){
-        return `svg version='1.1' xmlns`
-    }
-}
 
-inquirer.prompt([
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+}   
+
+
+const prompts = [
     {
         type: 'input',
         message: 'Enter up to 3 characters',
@@ -37,4 +45,4 @@ inquirer.prompt([
         choices: ['Circle', 'Square', 'Triangle'],
      
     },
-])
+];
