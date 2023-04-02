c5351aac4f4d8da418171835944a0ffeb83ee364
taylor-green <taylor.green2804@gmail.com>
2023-04-02 16:10:03 -0400

    created prompts for inqurier and began building constructor()

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {Circle, Square, Triangle} = require('./lib/shapes');
+
+class Svg{
+    constructor(){
+        this.textelement = ''
+        this.shapeElement = ''
+    }
+    render(){
+        return `svg version='1.1' xmlns`
+    }
+}
 
 inquirer.prompt([
     {
         type: 'input',
-        message: 'What shape do you want the logo to be',
-        name: 'title',
-        validate: (value) => {
-            if (value) {
-                return true;
-            } else {
-                return 'Input required to continue';
-            }
-        }
+        message: 'Enter up to 3 characters',
+        name: 'text'
+     
     },
     {
-
-    }
+        type: 'input',
+        message: 'What color would you like the font to be?',
+        name: 'text'
+    },
+    {
+        type: 'input',
+        message: 'What color would you like the shape to be?',
+        name: 'text'
+     
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like to use?',
+        name: 'pixel-image',
+        choices: ['Circle', 'Square', 'Triangle'],
+     
+    },
 ])
